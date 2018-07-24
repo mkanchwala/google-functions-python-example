f6a073e1523fa7befcf7ad31cc3ac13c86b812b5
murtaza.kanchwala <murtaza@kanchwala.in>
2018-07-24 21:59:50 +0530

    Added sample function for sklearn-numpy

---
+++ main.py
@@ -0,0 +1,41 @@
+print(__doc__)
+
+# Author : Murtaza Kanchwala
+
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+def getScore(request):
+    # Load the diabetes dataset
+    diabetes = datasets.load_diabetes()
+
+    # Use only one feature
+    diabetes_X = diabetes.data[:, np.newaxis, 2]
+
+    # Split the data into training/testing sets
+    diabetes_X_train = diabetes_X[:-20]
+    diabetes_X_test = diabetes_X[-20:]
+
+    # Split the targets into training/testing sets
+    diabetes_y_train = diabetes.target[:-20]
+    diabetes_y_test = diabetes.target[-20:]
+
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+
+    # Train the model using the training sets
+    regr.fit(diabetes_X_train, diabetes_y_train)
+
+    # Make predictions using the testing set
+    diabetes_y_pred = regr.predict(diabetes_X_test)
+
+    # The coefficients
+    print('Coefficients: \n', regr.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f"
+          % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
+    
+    return 'Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred)
